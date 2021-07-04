e7d638943892467c0a5b9e72e82466afb1225c4e
jogomez97 <jo_gomez97@hotmail.com>
2021-07-04 13:22:13 +0200

    Fixed broken menu links. Added default error page. Closes #45.

--- pet-clinic-web/src/main/java/jgomez/springframework/spring5petclinic/controllers/IndexController.java
+++ pet-clinic-web/src/main/java/jgomez/springframework/spring5petclinic/controllers/IndexController.java
@@ -16,4 +16,10 @@ public class IndexController {
     public String index() {
         return "index";
     }
+
+    @RequestMapping("/oups")
+    public String oups() {
+        return "error";
+    }
+
 }
--- pet-clinic-web/src/main/java/jgomez/springframework/spring5petclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/jgomez/springframework/spring5petclinic/controllers/OwnerController.java
@@ -26,4 +26,9 @@ public class OwnerController {
         return "owners/index";
     }
 
+    @RequestMapping({"/owners/find"})
+    public String findOwners() {
+        return "error";
+    }
+
 }
